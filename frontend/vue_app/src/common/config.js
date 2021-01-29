93f93570c41ed97556717b7dbb4e76ce9cf820ac
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-29 13:31:11 +0100

    changes

--- frontend/vue_app/src/common/config.js
+++ frontend/vue_app/src/common/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = "https://conduit.productionready.io/api";
-export const API_URL = "http://localhost:8080/";
+export const API_URL = "http://localhost:8001/";
 export default API_URL;
