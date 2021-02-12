ef830e15a73ceadbf2104be8226d8361780cc970
Alex Zarazua <alexzarazua88@gmail.com>
2021-02-12 15:43:51 +0100

    testing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'bars',
     'authentication',
     'core',
-    'profiles'
+    'profiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -72,6 +74,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Database
