45e8a20efe9c543092d42e2c71c820a82a51b0c3
viceentcb <viceentcb@gmail.com>
2021-02-16 20:11:40 +0100

    Documentation & change url's  authentication

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,41 +1,29 @@
-# from django.conf.urls import url
-
-# from .views import (
-#     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
-# )
-
-# urlpatterns = [
-#     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-#     url(r'^users/?$', RegistrationAPIView.as_view()),
-#     url(r'^users/login/?$', LoginAPIView.as_view()),
-# ]
 
 from django.conf.urls import include, url
 from django.urls import reverse
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView, UserViewSet
 )
+from rest_framework.routers import DefaultRouter
+
 
 app_name = 'authentication'
 
+router = DefaultRouter()
+
 #Admin
-user_list = UserViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+router.register(r'^userlist', UserViewSet)
+router.register(r'^userdetail', UserViewSet)
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+
+
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
     url(r'^users/?$', RegistrationAPIView.as_view()),
     url(r'^users/login/?$', LoginAPIView.as_view()),
 
-    #Admin
-    url(r'^userlist/$', user_list, name='user_list'),                                       
-    url(r'^userdetail/(?P<username>[0-9a-zA-Z_-]+)/$', user_detail, name='user_detail'), 
+    #Admin                               
+    # url(r'^userdetail/(?P<username>[0-9a-zA-Z_-]+)/$', UserViewSet.as_view( {'get': 'retrieve'} ))
 ]
